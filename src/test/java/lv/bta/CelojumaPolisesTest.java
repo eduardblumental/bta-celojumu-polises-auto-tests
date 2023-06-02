b3056c6a22ab0ea5ea0af4ac1b1692067ff17f8f
Eduards <eduard.blumental@gmail.com>
2023-06-02 21:28:19 +0300

    Destination selection and activity selection tests implemented.

--- src/test/java/lv/bta/CelojumaPolisesTest.java
+++ src/test/java/lv/bta/CelojumaPolisesTest.java
@@ -12,12 +12,16 @@ import static com.codeborne.selenide.Selenide.$;
 
 import org.openqa.selenium.By;
 
+import java.util.concurrent.TimeUnit;
+
 
 public class CelojumaPolisesTest {
     @BeforeClass
     public void setup() {
-        open("http://www.bta.lv");
+//        open("https://www.bta.lv");
+        open("https://www.bta.lv/privatpersonam/celojuma-apdrosinasana");
         $(By.xpath("//button[contains(text(), 'Piekrītu')]")).click();
+//        $(By.xpath("//h4[contains(text(), '  Ceļojumi  ')]")).shouldBe(visible).click();
     }
 
     @AfterClass
@@ -25,11 +29,35 @@ public class CelojumaPolisesTest {
         Selenide.closeWebDriver();
     }
 
-    @Test
-    public void testTravelInsurancePurchaseForm() {
-        $(By.xpath("//h4[contains(text(), '  Ceļojumi  ')]")).shouldBe(visible).click();
-        $("#regionalSelectorRegion-open").shouldBe(visible).click();
+    @Test(priority = 1)
+    public void testDestinationSelection() {
+        SelenideElement destinationSelectButton = $("#regionalSelectorRegion-open").shouldBe(visible);
+
+        String defaultDestination = destinationSelectButton.getText();
+
+        destinationSelectButton.click();
         $(By.xpath("//div[contains(text(), 'Izvēlies valstis ')]")).shouldBe(visible).click();
         $("#regionalSelectorCountry-addCountry").shouldBe(visible).click();
+        $(By.xpath("//button[@data-value='Indija']")).shouldBe(visible).click();
+        $("#regionalSelectorCountry-applyButton").shouldBe(visible).click();
+
+        String selectedDestination = $("#regionalSelectorRegion-open").shouldBe(visible).getText();
+
+        Assert.assertNotEquals(selectedDestination, defaultDestination);
+        Assert.assertEquals(selectedDestination, "Indija"); // This assert fails because the actual value is "Visa pasaule". Could be intentional, could be a bug.
+    }
+
+    @Test(priority = 2)
+    public void testActivitySelection() {
+        SelenideElement activitySelectButton = $("#travelActivities-open").shouldBe(visible);
+        String defaultActivity = activitySelectButton.getText();
+
+        activitySelectButton.click();
+        $("#travelActivities-popup-select-option-3").shouldBe(visible).click();
+
+        String selectedActivity = $("#travelActivities-open").shouldBe(visible).getText();
+
+        Assert.assertNotEquals(selectedActivity, defaultActivity);
+        Assert.assertEquals(selectedActivity, "Sports");
     }
 }
