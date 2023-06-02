f3e57ea228962c49a67b1dcaa98ceeb9ec3a2fe0
Eduards <eduard.blumental@gmail.com>
2023-06-02 19:30:04 +0300

    Necessary packages imported, navigating to travel insurance purchase form implemented.

--- src/test/java/lv/bta/CelojumaPolisesTest.java
+++ src/test/java/lv/bta/CelojumaPolisesTest.java
@@ -1,14 +1,35 @@
 package lv.bta;
 
-import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
-import static com.codeborne.selenide.Selenide.*;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.$;
+
+import org.openqa.selenium.By;
 
 
 public class CelojumaPolisesTest {
+    @BeforeClass
+    public void setup() {
+        open("http://www.bta.lv");
+        $(By.xpath("//button[contains(text(), 'Piekrītu')]")).click();
+    }
+
+    @AfterClass
+    public void tearDown() {
+        Selenide.closeWebDriver();
+    }
+
     @Test
-    public void boilerplateTest() {
-        System.out.println("Hello BTA!");
+    public void testTravelInsurancePurchaseForm() {
+        $(By.xpath("//h4[contains(text(), '  Ceļojumi  ')]")).shouldBe(visible).click();
+        $("#regionalSelectorRegion-open").shouldBe(visible).click();
+        $(By.xpath("//div[contains(text(), 'Izvēlies valstis ')]")).shouldBe(visible).click();
+        $("#regionalSelectorCountry-addCountry").shouldBe(visible).click();
     }
 }
