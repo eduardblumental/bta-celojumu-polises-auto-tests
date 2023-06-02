0fd11846d850be914b8fd98b8ef79784d56c8e9e
Eduards <eduard.blumental@gmail.com>
2023-06-02 17:48:07 +0300

    Boilerplate code for the tests created.

---
+++ src/main/java/lv/bta/Main.java
@@ -0,0 +1,7 @@
+package lv.bta;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello BTA!");
+    }
+}
---
+++ src/test/java/lv/bta/CelojumaPolisesTest.java
@@ -0,0 +1,14 @@
+package lv.bta;
+
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
+
+public class CelojumaPolisesTest {
+    @Test
+    public void boilerplateTest() {
+        System.out.println("Hello BTA!");
+    }
+}
