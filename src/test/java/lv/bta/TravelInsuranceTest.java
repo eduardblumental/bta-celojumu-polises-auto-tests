6a45ee1293889c20de538144113ec7f3ff2ad4c9
Eduards <eduard.blumental@gmail.com>
2023-06-03 21:04:50 +0300

    Popup form and traveler fields tests implemented.

--- src/test/java/lv/bta/TravelInsuranceTest.java
+++ src/test/java/lv/bta/TravelInsuranceTest.java
@@ -1,21 +1,21 @@
 package lv.bta;
 
-import org.testng.Assert;
-import org.testng.annotations.*;
-
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.ElementsCollection;
 
-import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 import org.openqa.selenium.By;
 
-import java.util.concurrent.TimeUnit;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 
-public class CelojumaPolisesTest {
+public class TravelInsuranceTest {
     @BeforeClass
     public void setup() {
 //        open("https://www.bta.lv");
@@ -42,7 +42,6 @@ public class CelojumaPolisesTest {
         $("#regionalSelectorCountry-applyButton").shouldBe(visible).click();
 
         String selectedDestination = $("#regionalSelectorRegion-open").shouldBe(visible).getText();
-
         Assert.assertNotEquals(selectedDestination, defaultDestination);
         Assert.assertEquals(selectedDestination, "Indija"); // This assert fails because the actual value is "Visa pasaule". Could be intentional, could be a bug.
     }
@@ -56,8 +55,33 @@ public class CelojumaPolisesTest {
         $("#travelActivities-popup-select-option-3").shouldBe(visible).click();
 
         String selectedActivity = $("#travelActivities-open").shouldBe(visible).getText();
-
         Assert.assertNotEquals(selectedActivity, defaultActivity);
         Assert.assertEquals(selectedActivity, "Sports");
+    }
+
+    @Test(priority = 3)
+    public void testPopupForm() {
+        $(By.xpath("//button[contains(text(), '  Saņemt piedāvājumu  ')]")).shouldBe(visible).click();
+
+        SelenideElement optimalProgramDiv = $(By.xpath("//div[@data-type='policyItemOPP']")).shouldBe(visible);
+        optimalProgramDiv.$(By.xpath(".//span[contains(text(), ' Apskati, kas ir apdrošināts')]")).click();
+
+        SelenideElement popupForm = $(".covered-popup-travel").shouldBe(visible);
+        Assert.assertTrue(popupForm.isDisplayed());
+
+        $(".popup-close.close").shouldBe(visible).click();
+    }
+
+    @Test(priority = 4)
+    public void testTravelerFields() {
+        $(By.xpath("//button[@datatype='selectPolicyPlanOPP']")).shouldBe(visible).click();
+
+        $(".traveler-details.flex.margin-top").shouldBe(visible);
+        ElementsCollection elements = $$(By.xpath("//input[@type='text']"));
+
+        for (SelenideElement element : elements) {
+            Assert.assertTrue(element.isDisplayed());
+            Assert.assertTrue(element.getValue().isEmpty());
+        }
     }
 }
